9814c15ed53ecd29777e8ce6563ce98e44de1971
shoamco <shoamc98@gmail.com>
2019-12-04 05:01:07 -0800

    add to controller run all appliction

---
+++ SRC/Controller/RunApplication.h
@@ -0,0 +1,41 @@
+//
+// Created by shoam on 12/4/19.
+//
+
+#ifndef TEMPDNA_RUNAPPLICATION_H
+#define TEMPDNA_RUNAPPLICATION_H
+
+#include "../View/CLI.h"
+class RunApplication{
+public:
+    void run();
+private:
+    /* map for all command,key-name of command ,value-pointer to function command*/
+    static CommandMap s_map_command;
+    static CLI s_cli;
+};
+static CommandMap init_map_command(){
+    /**Initialize map of commands */
+    CommandMap map;
+    map["new"]=new NewCreatorDnaSequence;
+    map["dup"]=new DupCreatorDnaSequence;
+    return map;
+}
+CommandMap RunApplication::s_map_command=init_map_command();/*Initialize map of command*/
+CLI RunApplication::s_cli=CLI();/*Initialize static  cli object*/
+
+void RunApplication::run(){
+    while (1) {
+//        std::cout << "enter command ";
+        VectorWords vectorWords=s_cli.get_input();
+        std::string command=vectorWords[0];
+        DnaSequence *dna=s_map_command[command]->CreateDnaSequence(vectorWords);
+//        DnaSequence *dna = newCreatorDnaSequence.CreateDnaSequence(vector_words[1]);
+        std::cout<<*dna;
+
+
+//        std::string input_line;
+//        getline(std::cin,input_line);
+    }
+}
+#endif //TEMPDNA_RUNAPPLICATION_H
