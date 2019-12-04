d316276955eff71e9dc29eba841d8ef9f1afb3c2
shoamco <shoamc98@gmail.com>
2019-12-04 05:07:03 -0800

    Organize all files

---
+++ SRC/View/Parser.h
@@ -0,0 +1,32 @@
+//
+// Created by shoam on 12/4/19.
+//
+
+#ifndef TEMPDNA_PARSSER_H
+#define TEMPDNA_PARSSER_H
+
+#include <vector>
+#include <string>
+
+typedef std::vector<std::string> VectorWords;
+
+//class Parser {
+//public:
+//
+//    Parser();
+//
+//private:
+//
+//    VectorWords vector_words;
+//};
+//Parser::Parser() {
+//
+//    std::stringstream ss(line); // Turn the string into a stream.
+//    while(getline(ss, tmp,' ')){
+//        if (tmp.compare(""))
+//            vector_words.push_back(tmp);
+//    }
+//
+//}
+
+#endif //TEMPDNA_PARSSER_H
