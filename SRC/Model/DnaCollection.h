6bb2e71ad3848d2c3cb4f5d02ac844429ec9c685
shoamco <shoamc98@gmail.com>
2019-12-05 03:48:05 -0800

    add function to ass dan record to maps collection

--- SRC/Model/DnaCollection.h
+++ SRC/Model/DnaCollection.h
@@ -5,4 +5,37 @@
 #ifndef TEMPDNA_DNACOLLECTION_H
 #define TEMPDNA_DNACOLLECTION_H
 
+#include <map>
+#include "DnaRecord.h"
+
+class DnaCollection {
+public:
+    void add_dna_to_map_name(DnaRecord *dnaRecord);
+
+    void add_dna_to_map_id(DnaRecord *dnaRecord);
+
+    void add_dna_to_maps(DnaRecord *dnaRecord);
+
+private:
+    std::map<std::string, DnaRecord *> map_dna_name;
+    std::map<size_t, DnaRecord *> map_dna_id;
+
+
+};
+//dnaCollection.map_dna_name[dnaRecord->getName()]=dnaRecord;//enter dna record to map collection
+//dnaCollection.map_dna_id[dnaRecord->getId()]=dnaRecord;
+
+void DnaCollection::add_dna_to_maps(DnaRecord *dnaRecord) {
+    add_dna_to_map_name(dnaRecord);
+    add_dna_to_map_id(dnaRecord);
+}
+
+void DnaCollection::add_dna_to_map_name(DnaRecord *dnaRecord) {
+    map_dna_name[dnaRecord->getName()] = dnaRecord;//enter dna record to map collection
+}
+
+void DnaCollection::add_dna_to_map_id(DnaRecord *dnaRecord) {
+    map_dna_id[dnaRecord->getId()] = dnaRecord;
+}
+
 #endif //TEMPDNA_DNACOLLECTION_H
