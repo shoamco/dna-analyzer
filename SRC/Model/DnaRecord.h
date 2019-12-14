f9da2b852f32e5878625d1848cc60d6b44d59f47
shoamco <shoamc98@gmail.com>
2019-12-14 10:43:43 -0800

    add get dna function

--- SRC/Model/DnaRecord.h
+++ SRC/Model/DnaRecord.h
@@ -8,36 +8,50 @@
 #include "DnaSequence.h"
 
 
-class DnaRecord{
+class DnaRecord {
 public:
     DnaRecord(DnaSequence *dna, std::string name, size_t id);
+
     friend std::ostream &operator<<(std::ostream &ostream, const DnaRecord dnaRecord);
+
 //    friend std::ostream &operator<<(std::ostream &ostream, const DnaRecord* dnaRecord);
     std::string getName() const;
-    size_t  getId() const;
+    DnaSequence *getDnaSequence() const;
+
+    size_t getId() const;
+
 private:
     DnaSequence *dnaSequence;
     std::string name;
     size_t id;
 
 };
-DnaRecord::DnaRecord(DnaSequence *dnaSequence, std::string name, size_t id): dnaSequence(dnaSequence), name(name), id(id){}
+inline  DnaSequence *DnaRecord::getDnaSequence() const{
+    return  dnaSequence;
+}
+inline DnaRecord::DnaRecord(DnaSequence *dnaSequence, std::string name, size_t id) : dnaSequence(dnaSequence),
+                                                                                     name(name), id(id) {}
+
 inline std::ostream &operator<<(std::ostream &ostream, const DnaRecord dnaRecord) {
     ostream << "DnaRecord: \n";
-    ostream<<dnaRecord.dnaSequence;
-    ostream << "name: "<<dnaRecord.name;
-    ostream << "\nid: "<<dnaRecord.id;
+    ostream << dnaRecord.dnaSequence;
+    ostream << "name: " << dnaRecord.name;
+    ostream << "\nid: " << dnaRecord.id;
 
     return ostream << std::endl;
 }
-inline std::ostream &operator<<(std::ostream &ostream, const DnaRecord* dnaRecord){
-    return operator<<(ostream,*dnaRecord);
+
+inline std::ostream &operator<<(std::ostream &ostream, const DnaRecord *dnaRecord) {
+    return operator<<(ostream, *dnaRecord);
 }
-inline std::string DnaRecord::getName() const{
+
+inline std::string DnaRecord::getName() const {
     return name;
 }
-inline size_t  DnaRecord::getId() const{
+
+inline size_t DnaRecord::getId() const {
     return id;
 
 }
+
 #endif //TEMPDNA_DNARECORD_H
